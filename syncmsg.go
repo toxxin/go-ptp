bbcbfd117fbd948f020a7ce375abc6edc052999d
Anton Glukhov <anton.a.glukhov@gmail.com>
2018-02-03 21:41:05 +0100

    add sync message implementation
    Signed-off-by: Anton Glukhov <anton.a.glukhov@gmail.com>

---
+++ syncmsg.go
@@ -0,0 +1,56 @@
+package ptp
+
+import (
+	"io"
+	"time"
+)
+
+// SyncMsg ...
+type SyncMsg struct {
+	Header
+	OriginTimestamp time.Time
+}
+
+// MarshalBinary allocates a byte slice and marshals a Frame into binary form.
+func (t *SyncMsg) MarshalBinary() ([]byte, error) {
+
+	if t.Header.MessageType != SyncMsgType {
+		return nil, ErrInvalidMsgType
+	}
+
+	if t.Header.MessageLength == 0 {
+		t.Header.MessageLength = HeaderLen + SyncPayloadLen
+	}
+
+	if t.Header.MessageLength != HeaderLen+SyncPayloadLen {
+		return nil, io.ErrUnexpectedEOF
+	}
+
+	b := make([]byte, HeaderLen+SyncPayloadLen)
+
+	headerSlice, err := t.Header.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	copy(b[:HeaderLen], headerSlice)
+
+	// Origin timestamp
+	tslice := time2OriginTimestamp(t.OriginTimestamp)
+	copy(b[HeaderLen:], tslice)
+
+	return b, nil
+}
+
+// UnmarshalBinary unmarshals a byte slice into a SyncMsg.
+//
+// If the byte slice does not contain enough data to unmarshal a valid SyncMsg,
+// io.ErrUnexpectedEOF is returned.
+func (t *SyncMsg) UnmarshalBinary(b []byte) error {
+	// Must contain type and length values
+	if len(b) < HeaderLen+SyncPayloadLen {
+		return io.ErrUnexpectedEOF
+	}
+
+	return ErrInvalidFrame
+}
