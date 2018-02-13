0d7a3d8d2f5338e27ba9d92f2a2c98ce09397d4a
Anton Glukhov <anton.a.glukhov@gmail.com>
2018-02-13 23:57:31 +0100

    add pdelrespmsg
    Signed-off-by: Anton Glukhov <anton.a.glukhov@gmail.com>

---
+++ pdelrespmsg.go
@@ -0,0 +1,61 @@
+package ptp
+
+import (
+	"encoding/binary"
+	"io"
+	"time"
+)
+
+// PDelRespMsg ...
+type PDelRespMsg struct {
+	Header
+	ReceiveTimestamp time.Time
+	ClockIdentity    uint64
+	PortID           uint16
+}
+
+// MarshalBinary allocates a byte slice and marshals a Frame into binary form.
+func (t *PDelRespMsg) MarshalBinary() ([]byte, error) {
+
+	if t.Header.MessageType != PDelayRespMsgType {
+		return nil, ErrInvalidMsgType
+	}
+
+	b := make([]byte, HeaderLen+PDelayRespPayloadLen)
+
+	headerSlice, err := t.Header.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	copy(b[:HeaderLen], headerSlice)
+	offset := HeaderLen
+
+	tmsSlice := time2OriginTimestamp(t.ReceiveTimestamp)
+	copy(b[offset:offset+10], tmsSlice)
+	offset += 10
+
+	clockIdentitySlice := make([]byte, 8)
+	binary.BigEndian.PutUint64(clockIdentitySlice, t.ClockIdentity)
+	copy(b[offset:offset+8], clockIdentitySlice)
+	offset += 8
+
+	portIDSlice := make([]byte, 2)
+	binary.BigEndian.PutUint16(portIDSlice, t.PortID)
+	copy(b[offset:offset+2], portIDSlice)
+
+	return b, nil
+}
+
+// UnmarshalBinary unmarshals a byte slice into a PDelRespMsg.
+//
+// If the byte slice does not contain enough data to unmarshal a valid PDelRespMsg,
+// io.ErrUnexpectedEOF is returned.
+func (t *PDelRespMsg) UnmarshalBinary(b []byte) error {
+	// Must contain type and length values
+	if len(b) < HeaderLen+PDelayRespPayloadLen {
+		return io.ErrUnexpectedEOF
+	}
+
+	return ErrInvalidFrame
+}
