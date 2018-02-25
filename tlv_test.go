463d8e14798e71bf2c22736146ce0200041caa46
Anton Glukhov <anton.a.glukhov@gmail.com>
2018-02-25 23:38:46 +0100

    add tests for PathTraceTlv MarshalBinary
    Signed-off-by: Anton Glukhov <anton.a.glukhov@gmail.com>

--- tlv_test.go
+++ tlv_test.go
@@ -1,12 +1,52 @@
 package ptp
 
 import (
+	"bytes"
 	"io"
 	"reflect"
 	"testing"
 )
 
 func TestMarshalPathTraceTlv(t *testing.T) {
+	var tests = []struct {
+		desc string
+		m    *PathTraceTlv
+		b    []byte
+		err  error
+	}{
+		{
+			desc: "Empty pathSequence",
+			m: &PathTraceTlv{
+				pathSequence: []uint64{},
+			},
+			b: append([]byte{0x0, 0x8, 0x0, 0x0}),
+		},
+		{
+			desc: "Single clockId in pathSequence",
+			m: &PathTraceTlv{
+				pathSequence: []uint64{0x0011223344556677},
+			},
+			b: append([]byte{0x0, 0x8, 0x0, 0x8,
+				0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77}),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			b, err := tt.m.MarshalBinary()
+			if err != nil {
+				if want, got := tt.err, err; want != got {
+					t.Fatalf("unexpected error: %v != %v", want, got)
+				}
+
+				return
+			}
+
+			if want, got := tt.b, b; !bytes.Equal(want, got) {
+				t.Fatalf("unexpected Frame bytes:\n- want: %#v\n-  got: %#v", want, got)
+			}
+		})
+	}
 }
 
 func TestUnmarshalPathTraceTlv(t *testing.T) {
