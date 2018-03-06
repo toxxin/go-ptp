34aa15ce0a596cb9b7e5e907deb7d91e649630bf
Anton Glukhov <anton.a.glukhov@gmail.com>
2018-03-06 23:41:42 +0100

    add test for UnmarshalBinary of SyncMsg struct
    Signed-off-by: Anton Glukhov <anton.a.glukhov@gmail.com>

--- syncmsg_test.go
+++ syncmsg_test.go
@@ -2,6 +2,7 @@ package ptp
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 	"time"
 )
@@ -55,4 +56,51 @@ func TestMarshalSync(t *testing.T) {
 }
 
 func TestUnmarshalSync(t *testing.T) {
+
+	var tests = []struct {
+		desc string
+		m    *SyncMsg
+		b    []byte
+		err  error
+	}{
+		{
+			desc: "Correct structure",
+			m: &SyncMsg{
+				Header: Header{
+					MessageType:      SyncMsgType,
+					MessageLength:    HeaderLen + SyncPayloadLen,
+					VersionPTP:       Version2,
+					CorrectionNs:     0,
+					CorrectionSubNs:  0,
+					ClockIdentity:    0x000af7fffe42a753,
+					PortNumber:       2,
+					SequenceID:       55330,
+					LogMessagePeriod: -4,
+				},
+				OriginTimestamp: time.Unix(500, 200),
+			},
+			b: append([]byte{0x0, 0x2, 0x0, 0x2c, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x0, 0x0, 0x0, 0x0,
+				0x0, 0xa, 0xf7, 0xff, 0xfe, 0x42, 0xa7, 0x53, 0x0, 0x2, 0xd8, 0x22, 0x0, 0xfc,
+				0x0, 0x0, 0x0, 0x0, 0x1, 0xf4, 0x0, 0x0, 0x0, 0xc8}),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			m := new(SyncMsg)
+			err := m.UnmarshalBinary(tt.b)
+			if err != nil {
+				if want, got := tt.err, err; want != got {
+					t.Fatalf("unexpected error: %v != %v", want, got)
+				}
+
+				return
+			}
+			if want, got := tt.m, m; !reflect.DeepEqual(want, got) {
+				t.Fatalf("unexpected Frame bytes:\n- want: %#v\n-  got: %#v", want, got)
+			}
+		})
+	}
 }
