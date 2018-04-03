9081fc15cd70b7a57877a10d9b80e5a5ccddcd70
Anton Glukhov <anton.a.glukhov@gmail.com>
2018-04-03 20:58:41 +0200

    add base unit test for TestUnmarshalSignaling func
    Signed-off-by: Anton Glukhov <anton.a.glukhov@gmail.com>

--- signalmsg_test.go
+++ signalmsg_test.go
@@ -2,6 +2,7 @@ package ptp
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -89,5 +90,61 @@ func TestMarshalSignaling(t *testing.T) {
 }
 
 func TestUnmarshalSignaling(t *testing.T) {
-	// TODO: add test
+	var tests = []struct {
+		desc string
+		m    *SignalingMsg
+		b    []byte
+		err  error
+	}{
+		{
+			desc: "Correct structure",
+			m: &SignalingMsg{
+				Header: Header{
+					MessageType:      SignalingMsgType,
+					MessageLength:    HeaderLen + SignalingPayloadLen + IntervalRequestTlvLen + 5,
+					VersionPTP:       Version2,
+					CorrectionNs:     0,
+					CorrectionSubNs:  0,
+					ClockIdentity:    0x001d7ffffe80024a,
+					PortNumber:       1,
+					SequenceID:       27278,
+					LogMessagePeriod: 127,
+				},
+				ClockIdentity: 0x78baf9fffe0a435e,
+				PortNumber:    1,
+				IntervalRequestTlv: IntervalRequestTlv{
+					LinkDelayInterval:        1,
+					TimeSyncInterval:         2,
+					AnnounceInterval:         127,
+					ComputeNeighborRateRatio: true,
+					ComputeNeighborPropDelay: false,
+				},
+			},
+			b: append([]byte{
+				0x0c, 0x02, 0x0, 0x38, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1d,
+				0x7f, 0xff, 0xfe, 0x80, 0x02, 0x4a, 0x00, 0x01, 0x6a, 0x8e, 0x05, 0x7f, 0x78, 0xba, 0xf9, 0xff,
+				0xfe, 0x0a, 0x43, 0x5e, 0x00, 0x01, 0x00, 0x03, 0x00, 0x0c, 0x0, 0x80, 0xc2, 0x0, 0x0, 0x2, 0x1, 0x2, 0x7f,
+				0x2, 0x0, 0x0,
+			}),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.desc, func(t *testing.T) {
+			m := new(SignalingMsg)
+			err := m.UnmarshalBinary(tt.b)
+			if err != nil {
+				if want, got := tt.err, err; want != got {
+					t.Fatalf("unexpected error: %v != %v", want, got)
+				}
+
+				return
+			}
+
+			if want, got := tt.m, m; !reflect.DeepEqual(want, got) {
+				t.Fatalf("unexpected Frame bytes:\n- want: %#v\n-  got: %#v", want, got)
+			}
+		})
+	}
 }
